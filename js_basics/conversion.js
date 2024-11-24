331a189791ad0da16106d6839523f97b3fdebf70
Pushkar-2301 <sikarwarpushkarsingh@gmail.com>
2024-11-24 10:17:46 +0000

    done

---
+++ js_basics/conversion.js
@@ -0,0 +1,52 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+let isloggedIn = 0 // if we pass  "string" => true 
+ let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "Pushakr" => true
+
+let someNumber = 21
+console.log( someNumber ,typeof someNumber);
+
+let stringNumber= String(someNumber);
+console.log( stringNumber ,typeof stringNumber);
+
+/* -----------Operations------------ */
+let value = 3
+let negative = -value
+console.log(negative);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1="hello"
+let str2="Pushkar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+ let num1 ,num2,num3
+ num1=num2=num3=2+4
+ console.log(num2);
+ 
