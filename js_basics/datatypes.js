868cea625b189ca1417f16b8abaaa6b8e88eec6b
Pushkar-2301 <sikarwarpushkarsingh@gmail.com>
2024-11-24 08:58:32 +0000

    done

---
+++ js_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as a newer version
+//alert() // we are using nodejs ,not browser
+console.log(3+
+    3) // code readability should be high
+    console.log("Pushkar");
+    let name='Pushkar'
+    let age =17
+    let isloggedin=false
+    let state;
+    // number => 2 to the power 53
+    // bigint
+    //string
+    // boolean
+    //null=> standalone value
+    //undefined =>
+   //symbol = unique
+ console.log(typeof undefined); //undefined
+ console.log(typeof null);
+ 
+     
---
+++ js_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId =21234
+var name="Pushkar"
+let accountGmail="abc@gmail.com"
+let accountPass="12334"
+city="Pune"
+console.log(accountId);
+
+let mobileno
+console.table([accountGmail,accountId,accountPass,city,mobileno])
